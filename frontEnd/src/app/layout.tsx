994b9685e650be7c2992521db26bb9612b5976c5
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-06-21 21:02:18 -0300

    change: altera linguagem e tema do Clerk.

--- frontEnd/src/app/layout.tsx
+++ frontEnd/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
+import { ptBR } from '@clerk/localizations'
+import { neobrutalism } from '@clerk/themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR} appearance={{ baseTheme: neobrutalism, elements: { formButtonPrimary: 'bg-violet-500 hover:bg-violet-600' } }}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <div className='h-screen flex justify-center items-center'>
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   )
